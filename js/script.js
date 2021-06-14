c786ee439db77d6e6e4390916032ce09b06ffb01
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-06-14 15:13:44 +0300

    made changes to the sex function

--- js/script.js
+++ js/script.js
@@ -20,46 +20,42 @@ function birthday(){
 
 function sex() {
     let gender; 
-    if ((form.gender[0].checked == true) || (form.gender[1].checked == true)) {
-        if (form.gender[0].checked == true) {
-            gender = 0;
-        } else if (form.gender[1].checked == true) {
-            gender = 1;
-        }
-        return gender;
-    } else {
-        alert("Please select your gender!");
-        return;
+
+    if (document.getElementById("male").checked) {
+        gender == 0
     }
+    else if(document.getElementById("female").checked){
+        gender == 1
+    }
+    else {
+        alert("PLEASE SELECT GENDER.")
+    }
+    // if ((form.gender[0].checked == true) || (form.gender[1].checked == true)) {
+    //     if (form.gender[0].checked == true) {
+    //         gender = 0;
+    //     } else if (form.gender[1].checked == true) {
+    //         gender = 1;
+    //     }
+    //     return gender;
+    // } else {
+    //     alert("Please select your gender!");
+    //     return;
+    // }
 }
 
 function selectDay() {
-    let selectedGender = sex;
-    let getBirthday = birthday;
+     let selectedGender = sex()
+     let getBirthday = birthday()
 
-    if (getBirthday.valueOf()===true || (getBirthday == 0)) {
-       if (selectedGender == 0) {
-        let submit = "Your Akan name is " + maleNames[getBirthday] + ".";
-        document.getElementById("submit").innerHTML = submit;
-        
-       }
-    }
-    else if(selectedGender == 1){
-        let submit = "Your Akan name is " + femaleNames[getBirthday] + ".";
-        document.getElementById("submit").innerHTML = submit;
-        
-    }
-    else{
-        return;
-    }
+if (getBirthday.valueOf() === true || (getBirthday === 0)) {
+     if (selectedGender == 0) {
+        let result = "Your Akan name is " + maleNames[getBirthday] + ".";
+        document.getElementById("submit").innerHTML = result;
+     }
+    //  else (selectedGender === 1){
+    //     let result = "Your Akan name is " + femaleNames[getBirthday] + ".";
+    //     document.getElementById("submit").innerHTML = result;
+    //  }
 }
 
-
-
-
-
-
-
-
-
-
+}
